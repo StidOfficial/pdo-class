fd0c28a70275f1747db979cc9bcdb336ba4a2ca2
StidOfficial <StidOfficial@users.noreply.github.com>
2016-03-25 11:34:44 +0100

    Update pdo.class.php
    Add Quote public function and Prepare Support

--- pdo.class.php
+++ pdo.class.php
@@ -1,6 +1,5 @@
 <?php
 class SQL {
-	
 	private $_PDO;
 	private $_PDO_DNS;
 	
@@ -8,9 +7,9 @@ class SQL {
 	
 	public function Connect() {
 		try {
-			$this->_PDO_CONFIG = parse_ini_file("sql.ini.php", true);
+			$this->_PDO_CONFIG = parse_ini_file("config.ini", true);
 			$this->_PDO_DNS = "mysql:host=".$this->_PDO_CONFIG["SQL"]["HOSTNAME"].";dbname=".$this->_PDO_CONFIG["SQL"]["DATABASE"];
-			$this->_PDO = new PDO($this->_PDO_DNS, $this->_PDO_CONFIG["SQL"]["USERNAME"], $this->_PDO_CONFIG["SQL"]["PASSWORD"], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+			$this->_PDO = new PDO($this->_PDO_DNS, $this->_PDO_CONFIG["SQL"]["USERNAME"], $this->_PDO_CONFIG["SQL"]["PASSWORD"], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING));
 		} catch (Exception $e) {
 			header("Content-Type: text/plain");
 			echo "[SQL][CONNECT] ", $e->getMessage();
@@ -19,14 +18,29 @@ class SQL {
 	}
 	
 	public function Query($_Query) {
-		$_Query_Result = $this->_PDO->query($_Query);
-		if($_Query_Result == false) {
+		$_SQL_Query = $this->_PDO->query($_Query);
+		if($_SQL_Query == false) {
 			header("Content-Type: text/plain");
 			echo "[SQL][QUERY] Query Error : ".$_Query;
 			die();
 		}else{
-			return $_Query_Result;
+			return $_SQL_Query;
 		}
+	}
+	
+	public function Prepare($_Prepare, $_Prepare_Parms) {
+		$_SQL_Prepare = $this->_PDO->prepare($_Prepare);
+		if($_SQL_Prepare == false) {
+			header("Content-Type: text/plain");
+			echo "[SQL][QUERY] Prepare Error : ".$_Prepare;
+			die();
+		}else{
+			return $_SQL_Prepare->execute($_Prepare_Parms);
+		}
+	}
+	
+	public function Quote($value) {
+		return $this->_PDO->quote($value);
 	}
 	
 	public function lastInsertId() {
